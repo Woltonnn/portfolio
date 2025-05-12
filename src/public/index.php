52d51bbbc3f9a2efd845e3bfa46c802eaa921ddd
Christian Woltinge <62013704+Woltonnn@users.noreply.github.com>
2025-05-12 23:47:46 +0200

    update filestructure

--- src/public/index.php
+++ src/public/index.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portfolio Christian</title>
+    <!-- <link rel="stylesheet" href="main.css"> -->
 </head>
 <body>
     <h1>This is my Portfolio</h1>
     <p>Some text!</p>
+
+    <!-- <script src="main.js"></script> -->
 </body>
 </html>
